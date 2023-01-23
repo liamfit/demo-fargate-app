6fc9cfed6de53036a60913ac0ce98f0650bb44a8
liamfit <lum.fitzpatrick@gmail.com>
2023-01-23 11:45:22 +0000

    updated the greeting

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello from ECS on Fargate";
+		return "Hello from ECS on Fargate - New Version!!";
 	}
 
 	public static void main(String[] args) {
