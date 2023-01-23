07fc1bdb703dd1ec48bc30d0a4b72c6d6a0fd4b4
liamfit <lum.fitzpatrick@gmail.com>
2023-01-23 11:53:28 +0000

    change it back

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello from ECS on Fargate - New Version!!";
+		return "Hello from ECS on Fargate";
 	}
 
 	public static void main(String[] args) {
